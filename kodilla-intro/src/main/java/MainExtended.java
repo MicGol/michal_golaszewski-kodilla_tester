29a0867ae912ab6a32d2da8a743516345ad3619a
Michal Golaszewski <michal.golaszewski.1996@gmail.com>
2022-10-13 18:22:45 +0200

    commit message FirstClass

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,22 @@
+public class MainExtended {
+    public static void main(String [] args) {
+        String myName = "Michał";
+        String myLastName = "Gołaszewski";
+        int myAge = 26;
+        double height = 1.89;
+        char schoolClass = 'F';
+        boolean isGoodProgrammer = true;
+        System.out.println("Jak masz na imię?");
+        System.out.println(myName);
+        System.out.println("Jak masz na nazwisko?");
+        System.out.println(myLastName);
+        System.out.println("W której klasie jesteś?");
+        System.out.println(schoolClass);
+        System.out.println("Jakiego jesteś wzrostu?");
+        System.out.println(height);
+        System.out.println("Ile masz lat?");
+        System.out.println(myAge);
+        System.out.println("Czy kiedyś możesz być dobrym programistą?");
+        System.out.println(isGoodProgrammer);
+    }
+}
